ba4acc7482e801e1dd51711dc538922ff9274978
Wouter <wouter@woutervandijke@.nl>
2017-09-02 13:01:53 +0200

    added ETH support

--- btc.py
+++ btc.py
@@ -4,18 +4,34 @@ from flask import Flask
 app = Flask(__name__)
 
 import requests
-def getbtc():
-	r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-	bitcoinprice = r.json()['bpi']['EUR']['rate_float']
-	myval = 0.04362136 * bitcoinprice
-	return "€"+str("%.2f" % round(myval,2))
 
+mybtcprice = ""
+myetherprice = ""
+
+def getcrypto():
+	r = requests.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=EUR')
+	getbtc(r)
+	geteth(r)
+
+def getbtc(r):
+	bitcoinprice = r.json()['BTC']['EUR']
+	mybtc = 0.04362136 * bitcoinprice
+	global mybtcprice
+	mybtcprice = "€"+str("%.2f" % round(mybtc,2))
+
+def geteth(r):
+	etherprice = r.json()['ETH']['EUR']
+	myether = 0.31298156 * etherprice
+	global myetherprice
+	myetherprice = "€" + str("%.2f" % round(myether, 2))
 
 @app.route("/")
 def hello():
-	html = "<div style=\"width: auto; margin:auto; margin-top: 50px; text-align: center;\"><h2>De waarde van mijn bitcoin is</h2><h1>"+getbtc()+"</h1></div>"
+	getcrypto()
+	html = "<div style=\"width: auto; margin:auto; margin-top: 50px; text-align: center;\"><h2>De waarde van mijn bitcoin is</h2><h1>"+mybtcprice+"</h1><h2>De waarde van mijn ethereum is</h2><h1>"+myetherprice+"</h1> (v=cryptocompare3)</div>"
 	return html
 
 @app.route("/raw")
 def rawbtc():
-	return getbtc()+"\r\n"
+	getcrypto()
+	return "BTC: "+mybtcprice+" ETH: "+myetherprice+"\r\n"
