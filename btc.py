f046d9bac758bf202615cdce3cbd143de14e528a
Wouter <wouter@woutervandijke@.nl>
2017-10-13 20:01:14 +0200

    Added price change

--- btc.py
+++ btc.py
@@ -21,18 +21,22 @@ def getbtc(r):
 	bitcoinprice = r.json()['BTC']['EUR']
 	mybtc = btcsetup * bitcoinprice
 	global mybtcprice
+	global btcchange
+	btcchange = "€" + str("%.2f" % round(mybtc - 100, 2))
 	mybtcprice = "€"+str("%.2f" % round(mybtc,2))
 
 def geteth(r):
 	etherprice = r.json()['ETH']['EUR']
 	myether = ethsetup * etherprice
 	global myetherprice
+	global ethchange
+	ethchange = "€" + str("%.2f" % round(myether - 100, 2))
 	myetherprice = "€" + str("%.2f" % round(myether, 2))
 
 @app.route("/btc")
 def hello():
 	getcrypto()
-	return render_template('btc.html', btcprice=mybtcprice, ethprice=myetherprice)
+	return render_template('btc.html', btcprice=mybtcprice, ethprice=myetherprice, btcchange=btcchange, ethchange=ethchange)
 
 @app.route("/raw")
 def rawbtc():
